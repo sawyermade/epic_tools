93c3c9bebc4b4d9b8514ed4f23b04952484e3345
sawyermade <sawyermade0@gmail.com>
2020-09-12 19:41:03 -0400

    added download script

---
+++ epic_download.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+temp_flag=true
+while $temp_flag; do
+	curl -LOvC - https://danielsawyer.com/epic_kitchens/2g1n6qdydwa9u22shpxqzp0t8m.zip && temp_flag=false
+done
+
+temp_flag=true
+while $temp_flag; do
+	curl -LOvC - https://danielsawyer.com/epic_kitchens/3h91syskeag572hl6tvuovwv4d.zip && temp_flag=false
+done
+
+temp_flag=true
+while $temp_flag; do
+	curl -LOvC - https://danielsawyer.com/epic_kitchens/masks/3l8eci2oqgst92n14w2yqi5ytu.zip && temp_flag=false
+done
+
+
---
+++ epic_unzip.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Place in same directory as zip files
+unzip 3h91syskeag572hl6tvuovwv4d.zip -d 3h91syskeag572hl6tvuovwv4d
+unzip 2g1n6qdydwa9u22shpxqzp0t8m.zip -d 2g1n6qdydwa9u22shpxqzp0t8m
