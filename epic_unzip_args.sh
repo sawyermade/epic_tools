704111ce3abc2ce03e976c2a28332a9d69092502
sawyermade <sawyermade0@gmail.com>
2020-09-12 20:38:23 -0400

    lazyu

--- epic_unzip_args.sh
+++ epic_unzip_args.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Pass arguments as directory where zips are located and director where zips should be extracted
-
-unzip $1/3h91syskeag572hl6tvuovwv4d.zip -d $2/3h91syskeag572hl6tvuovwv4d
-unzip $1/2g1n6qdydwa9u22shpxqzp0t8m.zip -d $2/2g1n6qdydwa9u22shpxqzp0t8m
+# Pass arguments as directory where zips are located and directory where zips should be extracted
+unzip $1/3h91syskeag572hl6tvuovwv4d.zip -d $2
+unzip $1/2g1n6qdydwa9u22shpxqzp0t8m.zip -d $2
