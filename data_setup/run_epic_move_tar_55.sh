6cc5b4d7558ee63bba58920bb92b503dda885554
sawyermade <sawyermade0@gmail.com>
2020-10-15 14:24:31 -0400

    lazy

--- data_setup/run_epic_move_tar_55.sh
+++ data_setup/run_epic_move_tar_55.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Extracts rgb tars
-python3 -u epic_copy_tar_55.py \
+python3 -u epic_move_tar_55.py \
 	~/epic_kitchens/3h91syskeag572hl6tvuovwv4d/frames_rgb_flow/rgb \
 	~/epic_kitchens/data_55/raw/rgb
 
 # Extracts flow tars
-python3 -u epic_copy_tar_55.py \
+python3 -u epic_move_tar_55.py \
 	~/epic_kitchens/3h91syskeag572hl6tvuovwv4d/frames_rgb_flow/flow \
 	~/epic_kitchens/data_55/raw/flow
