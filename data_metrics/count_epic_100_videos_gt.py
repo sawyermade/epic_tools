48a6c43ed7adeea0e35dee6698ef8e7bf540e247
sawyermade <sawyermade0@gmail.com>
2020-11-05 10:14:09 -0500

    lz

--- data_metrics/count_epic_100_videos_gt.py
+++ data_metrics/count_epic_100_videos_gt.py
@@ -1,8 +1,15 @@
-import sys
+import sys, os
 
 def main():
 	csv_path = sys.argv[1] # Path to gt file, ex: EPIC_100_train.csv
 	out_file = sys.argv[2] # output text file of your choice, ex: ./output_train.txt
+
+	# Makes output dirs if needed
+	out_split = out_file.split(os.sep)
+	if len(out_split) > 1 :
+		new_dir = os.path.join(out_split[:-1])
+		if not os.path.exists(new_dir):
+			os.makedirs(new_dir)
 
 	vid_set = set()
 	with open(csv_path) as cf:
