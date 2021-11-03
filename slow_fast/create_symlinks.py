bfef40442567475b277bbd07f4e99ef83d1ab9bb
sawyermade <sawyermade0@gmail.com>
2021-11-03 16:21:35 -0400

    slow fast symlinks

---
+++ slow_fast/create_symlinks.py
@@ -0,0 +1,44 @@
+import sys, os, re
+
+def main():
+	# Args
+	frame_dir = sys.argv[1]
+	symlink_dir = sys.argv[2]
+
+	# Get absolute paths
+	frame_dir = os.path.abspath(frame_dir)
+	symlink_dir = os.path.abspath(symlink_dir)
+	print(f'frame_dir, symlink_dir: {frame_dir}, {symlink_dir}')
+
+	# Create output symlink dir if it doesnt exist
+	if os.path.exists(symlink_dir):
+		os.system(f'rm -rf {symlink_dir}')
+	if not os.path.exists(symlink_dir):
+		os.makedirs(symlink_dir)
+
+	# Get participant dirs
+	reg_str = r'^P[\d]{2}$'
+	reg = re.compile(reg_str)
+	part_dirs = [d for d in os.listdir(frame_dir) if reg.match(d)]
+	print(part_dirs)
+
+	part_paths = [os.path.join(frame_dir, d) for d in part_dirs]
+	part_paths = [d for d in part_paths if os.path.isdir(d)]
+	print(part_paths)
+
+	# Create symlinks
+	reg_str = r'^P[\d]{2}_[\d]{2,3}$'
+	reg = re.compile(reg_str)
+	for part in part_paths:
+		# Get directories in part path
+		parts = os.listdir(part)
+		parts = [d for d in parts if reg.match(d)]
+		parts = [os.path.join(part, d) for d in parts]
+
+		# Create links
+		for p in parts:
+			os.system(f'ln -s {p} {symlink_dir}')
+
+
+if __name__ == '__main__':
+	main()
